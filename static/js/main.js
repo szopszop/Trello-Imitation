5eb20a2c7743498ff35f28bea84f1f269852d925
appuser <appuser@journey2-b495b67bf-shvrv>
2022-06-27 07:11:15 +0000

    starter project

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
+import { dom } from "./dom.js";
+
+// This function is to initialize the application
+function init() {
+    // init data
+    dom.init();
+    // loads the boards to the screen
+    dom.loadBoards();
+
 import {boardsManager} from "./controller/boardsManager.js";
 
 function init() {
