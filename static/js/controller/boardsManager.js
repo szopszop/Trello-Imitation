9680da11ef55f51576e8396951e7826dc0c4aba3
Szymon Tracz <szymontracz1@gmail.com>
2022-06-28 15:45:22 +0200

    hfg

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -2,6 +2,8 @@ import {dataHandler} from "../data/dataHandler.js";
 import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
 import {domManager} from "../view/domManager.js";
 import {cardsManager} from "./cardsManager.js";
+import {socket} from "../main.js";
+
 
 export let boardsManager = {
     loadBoards: async function () {
